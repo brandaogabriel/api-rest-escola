440f0befbc76e326509020705e284f74463f550b
brandaogabriel <gabriel.brandao@ccc.ufcg.edu.br>
2020-12-08 15:29:38 -0300

    config sequelize e CRUD Users

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,8 +1,16 @@
+import Aluno from '../models/Aluno';
+
 class HomeController {
-  index(req, res) {
-    res.json({
-      tudoCerto: true,
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: 'Ana',
+      sobrenome: 'Caroline',
+      email: 'anacaroline@nunesmaciel',
+      idade: 20,
+      peso: 50,
+      altura: 1.55,
     });
+    res.json(novoAluno);
   }
 }
 

15f99026f853096e79601fe44d09fcc02ffb5e7d
brandaogabriel <gabriel.brandao@ccc.ufcg.edu.br>
2020-12-08 10:18:51 -0300

    estrutura do projeto

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+    'class-methods-use-this': 'off',
+  },
+};

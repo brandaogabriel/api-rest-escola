e57ea4e7f3688f9e935da31e6809b725b05cf384
brandaogabriel <gabriel.brandao@ccc.ufcg.edu.br>
2020-12-10 11:06:39 -0300

    config para deploy

--- server.js
+++
@@ -1,8 +0,0 @@
-import app from './app';
-
-const port = 3001;
-app.listen(port, () => {
-  console.log();
-  console.log(`Aplicação está ativa na porta ${port} 🚀`);
-  console.log(`CTRL + Clique em http://localhost:${port}`);
-});

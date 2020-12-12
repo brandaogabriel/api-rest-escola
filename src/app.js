b105a46208b8f297f27b73ae0d4703cfc4118fd7
brandaogabriel <gabriel.brandao@ccc.ufcg.edu.br>
2020-12-12 12:07:15 -0300

    Adicionado cors

--- src/app.js
+++ src/app.js
@@ -1,15 +1,30 @@
-import dotenv from 'dotenv';
 import { resolve } from 'path';
-
-dotenv.config();
 
 import './database';
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+
 import homeRoutes from './routes/homeRoutes';
 import userRoutes from './routes/userRoutes';
 import tokenRoutes from './routes/tokenRoutes';
 import alunoRoutes from './routes/alunoRoutes';
 import fotoRoutes from './routes/fotoRoutes';
+
+const whiteList = [
+  `react2.${process.env.APP_URL}`,
+  'http://localhost:3001',
+];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not alloweb by CORS'));
+    }
+  },
+};
 
 class App {
   constructor() {
@@ -19,6 +34,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use('/images/', express.static(resolve(__dirname, '..', 'uploads', 'images')));
